fd5d0db6ce51948974ddbeab9686f7549734a584
Tyler Barber <tylerwbarber@gmail.com>
2015-11-29 17:47:20 -0600

    Test 23 Passing - Compund tasks can get pretty gross.

--- src/ii_collections/_23_CompoundTasks.kt
+++ src/ii_collections/_23_CompoundTasks.kt
@@ -4,23 +4,17 @@ import ii_collections.data.customers
 import ii_collections.data.orderedProducts
 
 fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
-    return customers.filter {
-        it.orderedProducts.contains(product)
-    }.toSet()
+    return customers.filter { it.orderedProducts.contains(product) }.toSet()
 }
 
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
     // Return the most expensive product among all delivered products
     // (use the Order.isDelivered flag)
-    return customers.filter {
-        orderedProducts.
-    }
+    return orders.filter { it.isDelivered }.flatMap { it.products }.maxBy { it.price }
 }
 
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
     // Return the number of times the given product was ordered.
     // Note: a customer may order the same product for several times.
-    return customers.filter {
-        orderedProducts.
-    }.count {  }
+    return customers.flatMap { it.orders.flatMap { it.products }}.count { it == product }
 }
