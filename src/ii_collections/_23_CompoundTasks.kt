d9feb825743c866baacc117604f35db814e65c1d
Tyler Barber <tylerwbarber@gmail.com>
2015-11-29 16:50:49 -0600

    Test 23 - In Progress

--- src/ii_collections/_23_CompoundTasks.kt
+++ src/ii_collections/_23_CompoundTasks.kt
@@ -1,18 +1,26 @@
 package ii_collections
 
+import ii_collections.data.customers
+import ii_collections.data.orderedProducts
+
 fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
-    // Return the set of customers who ordered the specified product
-    todoCollectionTask()
+    return customers.filter {
+        it.orderedProducts.contains(product)
+    }.toSet()
 }
 
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
     // Return the most expensive product among all delivered products
     // (use the Order.isDelivered flag)
-    todoCollectionTask()
+    return customers.filter {
+        orderedProducts.
+    }
 }
 
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
     // Return the number of times the given product was ordered.
     // Note: a customer may order the same product for several times.
-    todoCollectionTask()
+    return customers.filter {
+        orderedProducts.
+    }.count {  }
 }
